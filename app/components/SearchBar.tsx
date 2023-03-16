fc84c6362e9b5ac91378da440cd5596463276469
DevNdy <developpeur.ndy@gmail.com>
2023-03-16 11:38:47 +0100

    F01 feat SEARCHBAR

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className='flex items-center m-3'>
+            <div className='flex flex-row items-center bg-slate-100 p-2 rounded-lg w-[280px]'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+                <input type="text" className="w-full bg-slate-100 h-[35px] ml-2 pl-2 focus:outline-slate-200"/>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 ml-2 bg-slate-700 text-white p-2 rounded-lg">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+            </svg>
+
+        </div>
+    );
+};
+
+export default SearchBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import {BeakerIcon} from '@heroicons/react/24/solid'
+import SearchBar from "@/app/components/SearchBar";
 
 export default function Home() {
     return (
         <main className='flex flex-col m-3'>
             <h1 className='text-3xl text-slate-800 font-bold mt-12 m-3'>Pokédex</h1>
-            <p className='mt-3 m-3 text-slate-500'>Rechercher un pokémon avec le nom ou utiliser son Numéro National Pokédex </p>
+            <p className='mt-3 m-3 text-slate-500'>Rechercher un pokémon avec le nom ou utiliser son Numéro National Pokédex
+            </p>
+            <SearchBar/>
         </main>
     )
 }
